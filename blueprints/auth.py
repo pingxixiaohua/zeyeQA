e1157a5a2be7cb722dbc69f54bf7e6e487be657a
tingyu <1608386461@qq.com>
2025-05-02 16:41:29 +0800

    完善登录注册页面功能

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,19 +1,41 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, redirect, url_for, session
 from exts import mail,db
 from flask_mail import Message
 from flask import request
 import random
 import string
 from models import EmailCaptchaModel
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
+from models import UserModel
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # /auth开头
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 # /auth/login
-@bp.route('/login')
+@bp.route('/login', methods = ['GET', 'POST'])
 def login():
-    pass
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        form = LoginForm(request.form)
+        if form.validate():
+            email = form.email.data
+            password = form.password.data
+            user = UserModel.query.filter_by(email=email).first()
+            if not user:
+                print("邮箱不存在")
+                return redirect(url_for("auth.login"))
+            if check_password_hash(user.password, password):
+                # session
+                session['user_id'] = user.id
+                return redirect("/")
+            else:
+                print("密码错误")
+                return redirect(url_for("auth.login"))
+        else:
+            print(form.errors)
+            return redirect(url_for("auth.login"))
 
 @bp.route('/register', methods=["GET","POST"])
 def register():
@@ -23,10 +45,19 @@ def register():
         # 表单验证：flask-wtf
         form = RegisterForm(request.form)
         if form.validate():
-            return "success"
+            email = form.email.data
+            username = form.username.data
+            password = form.password.data
+            password_confirm = form.password_confirm.data
+            print(password,password_confirm)
+            user = UserModel(email=email, username=username, password=generate_password_hash(password))
+            db.session.add(user)
+            db.session.commit()
+            # return redirect("/auth/login")
+            return redirect(url_for("auth.login"))
         else:
             print(form.errors)
-            return "fail"  
+            return redirect(url_for("auth.register"))
 
 @bp.route('/cpatcha/email')
 def get_email_cpatcha():
--- blueprints/forms.py
+++ blueprints/forms.py
@@ -8,7 +8,7 @@ class RegisterForm(wtforms.Form):
     captcha = wtforms.StringField(validators=[Length(min=4, max=4, message="验证码格式错误")])
     username = wtforms.StringField(validators=[Length(min=4, max=20, message="用户名错误")])
     password = wtforms.StringField(validators=[Length(min=6, max=20, message="密码格式错误")])
-    password_config = wtforms.StringField(validators=[EqualTo("password", message="两次密码不一致")])
+    password_confirm = wtforms.StringField(validators=[EqualTo("password", message="两次密码不一致")])
 
     # 自定义验证器
     # 1、验证邮箱是否已经被注册
@@ -29,3 +29,7 @@ class RegisterForm(wtforms.Form):
         # else: 
         #     db.session.delete(captcha_model)
         #     db.session.commit()
+
+class LoginForm(wtforms.Form):
+    email = wtforms.StringField(validators=[Email(message="邮箱格式错误！")])
+    password = wtforms.StringField(validators=[Length(min=6, max=20, message="密码格式错误")])
